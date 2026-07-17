6ab7bb75405bfbf2e90bcba85d7def415c4f06da
JoseLuis <joseluishernandeztareas@gmail.com>
2026-07-17 14:14:58 -0700

    Fix Stripe API version to 2026-06-24.dahlia

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-11-20.acacia" as unknown as Stripe.LatestApiVersion,
+  apiVersion: "2026-06-24.dahlia" as any,
 });
 
 export const PRICE_IDS = {
